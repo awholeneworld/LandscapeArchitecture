87e60c6b2085deb8fda63632ddd22225944517a5
JM2308 <wlals2987@naver.com>
2021-04-26 21:45:37 +0900

    add free, review, tip community and connect listStyleFrame and communityFrame

---
+++ app/src/main/java/gachon/termproject/joker/TipCommunity.java
@@ -0,0 +1,20 @@
+package gachon.termproject.joker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TipCommunity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tip_community, container, false);
+        return view;
+    }
+}
