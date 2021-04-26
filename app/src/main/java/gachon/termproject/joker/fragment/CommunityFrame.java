199e57592947b72b20dcf6fd9724ee0ba12402bb
JM2308 <wlals2987@naver.com>
2021-04-26 21:12:57 +0900

    add free & review & tip community and connect listStyleFrame & communityFrame

--- app/src/main/java/gachon/termproject/joker/fragment/CommunityFrame.java
+++ app/src/main/java/gachon/termproject/joker/fragment/CommunityFrame.java
@@ -1,66 +1,122 @@
 package gachon.termproject.joker.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+import gachon.termproject.joker.FreeCommunity;
 import gachon.termproject.joker.R;
+import gachon.termproject.joker.ReviewCommunity;
+import gachon.termproject.joker.TipCommunity;
+
+import static android.app.Activity.RESULT_OK;
 
 public class  CommunityFrame extends Fragment {
     private View view;
     private FragmentManager fm;
-    private FloatingActionButton button1;
-    private FloatingActionButton button2;
+    private FreeCommunity free;
+    private ReviewCommunity review;
+    private TipCommunity tip;
     private CommunityListStyle communityList;
     private CommunityAlbumStyle communityAlbum;
+
+    TabLayout tabs;
+    Button selectCommunityMode;
+    boolean i = true;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.community_frame, container, false);
 
-        fm = getChildFragmentManager();
-        if (communityList == null) {
-            communityList = new CommunityListStyle();
-            fm.beginTransaction().add(R.id.community_frame, communityList).commit();
-            fm.beginTransaction().show(communityList).commit();
-        }
+        TabLayout tabLayout = view.findViewById(R.id.tabs);
+        tabs = view.findViewById(R.id.tabs) ;
 
-        // <현재 테스트용 코드>
-        // button1 = 리스트 & button2 = 앨범형
-        // 추후 탭 버튼이랑 연계하면 됨
-        button1 = view.findViewById(R.id.list);
-        button1.setOnClickListener(new View.OnClickListener() {
+        FreeCommunity freeCommunity = new FreeCommunity();
+        ReviewCommunity reviewCommunity = new ReviewCommunity();
+        TipCommunity tipCommunity = new TipCommunity();
+
+        fm = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.contents, new FreeCommunity());
+        fragmentTransaction.commit();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                if (communityAlbum != null) {
-                    fm.beginTransaction().hide(communityAlbum).commit();
-                    fm.beginTransaction().show(communityList).commit();
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch(tab.getPosition()) {
+                    case 0 :
+                        fm.beginTransaction().replace(R.id.contents, new FreeCommunity()).commit();
+                        break ;
+                    case 1 :
+                        fm.beginTransaction().replace(R.id.contents, new ReviewCommunity()).commit();
+                        break ;
+                    case 2 :
+                        fm.beginTransaction().replace(R.id.contents, new TipCommunity()).commit();
+                        break ;
                 }
             }
-        });
 
-        button2 = view.findViewById(R.id.album);
-        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+
+        }) ;
+
+        // 앨범,리스트 변환부분
+        selectCommunityMode = view.findViewById(R.id.selectCommunityMode);
+        selectCommunityMode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (communityAlbum == null) {
-                    communityAlbum = new CommunityAlbumStyle();
-                    fm.beginTransaction().add(R.id.community_frame, communityAlbum).commit();
+                if (i == true){
+                    fm.beginTransaction().replace(R.id.contents, new CommunityListStyle()).commit();
+                    selectCommunityMode.setBackgroundResource(R.drawable.ic_baseline_photo_library_24);
+                    i = false;
+                } else {
+                    fm.beginTransaction().replace(R.id.contents, new CommunityAlbumStyle()).commit();
+                    selectCommunityMode.setBackgroundResource(R.drawable.ic_baseline_view_list_24);
+                    i = true;
                 }
-                fm.beginTransaction().show(communityAlbum).commit();
-                fm.beginTransaction().hide(communityList).commit();
             }
         });
 
         return view;
     }
+
 }
