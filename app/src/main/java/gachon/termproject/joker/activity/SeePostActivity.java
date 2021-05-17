73b84f9612fca5c338f5e7c770437549eabbb795
smpark0213 <smpark0213@naver.com>
2021-05-18 01:55:45 +0900

    PostContent.java의 데이터량(2개) 증가 적용

--- app/src/main/java/gachon/termproject/joker/activity/SeePostActivity.java
+++ app/src/main/java/gachon/termproject/joker/activity/SeePostActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
