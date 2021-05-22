90ccac70de35e659c735483f7afd5a448cf53ec1
awholeneworld <blackjjang12@gmail.com>
2021-05-23 01:20:03 +0900

    Enable myinfo post auto-update

--- app/src/main/java/gachon/termproject/joker/adapter/MyInfoTabPostAdapter.java
+++ app/src/main/java/gachon/termproject/joker/adapter/MyInfoTabPostAdapter.java
@@ -33,19 +33,152 @@ import gachon.termproject.joker.activity.MainActivity;
 import gachon.termproject.joker.activity.SeePostActivity;
 import gachon.termproject.joker.Content.PostContent;
 
-
 public class MyInfoTabPostAdapter extends RecyclerView.Adapter<MyInfoTabPostAdapter.ViewHolder> {
     private Context context;
     private ArrayList<PostContent> myInfoPostList;
-    private ArrayList<String> userPostsIdList;
     private int finishCount = 0;
 
     public MyInfoTabPostAdapter(Context context) {
         this.context = context;
         myInfoPostList = new ArrayList<>();
-        //userPostsIdList = new ArrayList<>();
 
-        /*
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+        dbRef.get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
+                final long categoryNum = dataSnapshot.getChildrenCount();
+                dbRef.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull @NotNull DataSnapshot snapshot, @Nullable @org.jetbrains.annotations.Nullable String previousChildName) {
+                        snapshot.getRef().orderByChild("userId").equalTo(UserInfo.userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @RequiresApi(api = Build.VERSION_CODES.N)
+                            @Override
+                            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                                for (DataSnapshot item : snapshot.getChildren()) {
+                                    PostContent myInfoPostContent = item.getValue(PostContent.class);
+                                    myInfoPostList.add(0, myInfoPostContent);
+                                    MainActivity.userPostsIdList.add(0, myInfoPostContent.getPostId());
+                                }
+
+                                finishCount++;
+                                if (finishCount == categoryNum) {
+                                    myInfoPostList.sort(new Comparator<PostContent>() {
+                                        @RequiresApi(api = Build.VERSION_CODES.O)
+                                        @Override
+                                        public int compare(PostContent o1, PostContent o2) {
+                                            long o1Id = Long.parseUnsignedLong(o1.getPostId());
+                                            long o2Id = Long.parseUnsignedLong(o2.getPostId());
+
+                                            if (o1Id < o2Id) return 1;
+                                            else return -1;
+                                        }
+                                    });
+
+                                    notifyDataSetChanged();
+                                    finishCount = 0;
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull @NotNull DataSnapshot snapshot, @Nullable @org.jetbrains.annotations.Nullable String previousChildName) {
+                        myInfoPostList.clear();
+                        snapshot.getRef().orderByChild("userId").equalTo(UserInfo.userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @RequiresApi(api = Build.VERSION_CODES.N)
+                            @Override
+                            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                                for (DataSnapshot item : snapshot.getChildren()) {
+                                    PostContent myInfoPostContent = item.getValue(PostContent.class);
+                                    myInfoPostList.add(0, myInfoPostContent);
+                                    MainActivity.userPostsIdList.add(0, myInfoPostContent.getPostId());
+                                }
+
+                                finishCount++;
+                                if (finishCount == categoryNum) {
+                                    myInfoPostList.sort(new Comparator<PostContent>() {
+                                        @RequiresApi(api = Build.VERSION_CODES.O)
+                                        @Override
+                                        public int compare(PostContent o1, PostContent o2) {
+                                            long o1Id = Long.parseUnsignedLong(o1.getPostId());
+                                            long o2Id = Long.parseUnsignedLong(o2.getPostId());
+
+                                            if (o1Id < o2Id) return 1;
+                                            else return -1;
+                                        }
+                                    });
+
+                                    notifyDataSetChanged();
+                                    finishCount = 0;
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull @NotNull DataSnapshot snapshot) {
+                        myInfoPostList.clear();
+                        snapshot.getRef().orderByChild("userId").equalTo(UserInfo.userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @RequiresApi(api = Build.VERSION_CODES.N)
+                            @Override
+                            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                                for (DataSnapshot item : snapshot.getChildren()) {
+                                    PostContent myInfoPostContent = item.getValue(PostContent.class);
+                                    myInfoPostList.add(0, myInfoPostContent);
+                                    MainActivity.userPostsIdList.add(0, myInfoPostContent.getPostId());
+                                }
+
+                                finishCount++;
+                                if (finishCount == categoryNum) {
+                                    myInfoPostList.sort(new Comparator<PostContent>() {
+                                        @RequiresApi(api = Build.VERSION_CODES.O)
+                                        @Override
+                                        public int compare(PostContent o1, PostContent o2) {
+                                            long o1Id = Long.parseUnsignedLong(o1.getPostId());
+                                            long o2Id = Long.parseUnsignedLong(o2.getPostId());
+
+                                            if (o1Id < o2Id) return 1;
+                                            else return -1;
+                                        }
+                                    });
+
+                                    notifyDataSetChanged();
+                                    finishCount = 0;
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull @NotNull DataSnapshot snapshot, @Nullable @org.jetbrains.annotations.Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
+        /* 이 코드는 추후 개선 목적으로 작성 중에 있는 코드입니다.
+        // 로그인 할 때 자기가 쓴 포스트 아이디 리스트를 MainActivity로 보내게 되는데 그 아이디 리스트가지고 게시물 불러오는게 좀 더 효율적이지 않을까 싶어서
         for (String myPostId : MainActivity.userPostsIdList) {
             FirebaseDatabase.getInstance().getReference().child("Posts").orderByChild(myPostId).addChildEventListener(new ChildEventListener() {
                 @Override
@@ -80,73 +213,6 @@ public class MyInfoTabPostAdapter extends RecyclerView.Adapter<MyInfoTabPostAdap
             });
         }
         */
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("Posts");
-        dbRef.get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
-            @Override
-            public void onSuccess(DataSnapshot dataSnapshot) {
-                final long categoryNum = dataSnapshot.getChildrenCount();
-                dbRef.addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull @NotNull DataSnapshot snapshot, @Nullable @org.jetbrains.annotations.Nullable String previousChildName) {
-                        snapshot.getRef().orderByChild("userId").equalTo(UserInfo.userId).addListenerForSingleValueEvent(new ValueEventListener() {
-                            @RequiresApi(api = Build.VERSION_CODES.N)
-                            @Override
-                            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                                for (DataSnapshot item : snapshot.getChildren()) {
-                                    PostContent myInfoPostContent = item.getValue(PostContent.class);
-                                    myInfoPostList.add(0, myInfoPostContent);
-                                    userPostsIdList.add(0, myInfoPostContent.getPostId());
-                                }
-
-                                finishCount++;
-                                if (finishCount == categoryNum) {
-                                    myInfoPostList.sort(new Comparator<PostContent>() {
-                                        @RequiresApi(api = Build.VERSION_CODES.O)
-                                        @Override
-                                        public int compare(PostContent o1, PostContent o2) {
-                                            long o1Id = Long.parseUnsignedLong(o1.getPostId());
-                                            long o2Id = Long.parseUnsignedLong(o2.getPostId());
-
-                                            if (o1Id < o2Id) return 1;
-                                            else return -1;
-                                        }
-                                    });
-
-                                    MainActivity.userPostsIdList = userPostsIdList;
-                                    notifyDataSetChanged();
-                                    finishCount = 0;
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onChildChanged(@NonNull @NotNull DataSnapshot snapshot, @Nullable @org.jetbrains.annotations.Nullable String previousChildName) {
-
-                    }
-
-                    @Override
-                    public void onChildRemoved(@NonNull @NotNull DataSnapshot snapshot) {
-
-                    }
-
-                    @Override
-                    public void onChildMoved(@NonNull @NotNull DataSnapshot snapshot, @Nullable @org.jetbrains.annotations.Nullable String previousChildName) {
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-                    }
-                });
-            }
-        });
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
